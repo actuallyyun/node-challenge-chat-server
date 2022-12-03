e17b04520eef16648f54d15c46ad3b96bb14c932
Yun Ji <this.jiyun@gmail.com>
2022-12-03 13:50:17 +0100

    seperate projects to be and fe

---
+++ fe/src/App.js
@@ -0,0 +1,26 @@
+import logo from './logo.svg';
+import './App.css';
+import React, { UseEffect } from "react"
+
+function App() {
+  return (
+    <div className="App">
+
+      <div className="container">
+        <div className="row">
+          <div className="col">
+            <h2>Latest Messages</h2>
+            <p>Messages</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+
+
+const LatestMessages = () => {
+
+}
+export default App;
