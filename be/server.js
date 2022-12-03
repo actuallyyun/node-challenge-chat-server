8b9e7d02cf13c39d3f4b5e9d052045bcdeb9eeb2
Yun Ji <this.jiyun@gmail.com>
2022-12-03 14:33:44 +0100

    refactor all level 1& 2 routes

--- be/server.js
+++ be/server.js
@@ -14,28 +14,80 @@ const welcomeMessage = {
 
 const messages = [welcomeMessage];
 
-app.get("/", function (request, response) {
-  response.sendFile(__dirname + "/index.html");
-});
 
-
-
-app.get("/messages/:id", (req, res) => {
+const getMessageById = (req, res) => {
   const id = req.params.id
   const message = messages.find(message => message.id == id)
+  const statusCode = message ? 200 : 400
+  res.status(statusCode).send(message)
+}
 
-  res.send(message)
 
-})
-
-app.put("/messages/:id", (req, res) => {
+const updateMessageById = (req, res) => {
   const id = req.params.id
   const message = messages.find(message => message.id == id)
   message.text = req.body.text
   message.from = req.body.from
 
   res.send(message)
-})
+
+}
+
+const deleteMessageById = (req, res) => {
+  const msgId = req.params.id
+  const indexToDelete = messages.findIndex(message => message.id == msgId)
+  const foundIt = indexToDelete != -1
+
+  const statusCode = foundIt ? 200 : 404
+  const deletedMessage = foundIt ? (messages[indexToDelete]) : (null)
+
+  if (foundIt) {
+    messages.splice(indexToDelete, 1)
+  }
+
+  res.status(statusCode).send(deletedMessage)
+
+}
+
+const creatNewMessage = (req, res) => {
+  const isValid = validMessage(req.body)
+
+  const statusCode = isValid ? 201 : 400
+  let createdMessage
+
+  if (isValid) {
+    const messageContent = req.body
+    const timeSent = new Date()
+    const newMessage = { id: messages.length, from: messageContent.from, text: messageContent.text, timeSent: timeSent }
+    messages.push(newMessage)
+    res.send(newMessage)
+    createdMessage = newMessage
+
+  }
+  res.status(statusCode).send(createdMessage)
+}
+
+const validMessage = (message) => {
+  if (message.from === undefined || message.text === undefined || message.from === "" || message.text === "") {
+    return false
+  } else {
+    return true
+  }
+}
+
+
+app.get("/", function (request, response) {
+  response.sendFile(__dirname + "/index.html");
+});
+
+
+app.get("/messages/:id", getMessageById)
+
+app.put("/messages/:id", updateMessageById)
+
+app.delete("/messages/:id", deleteMessageById)
+
+app.post("/messages", creatNewMessage)
 
 app.get("/messages", (req, res) => {
 
@@ -49,7 +101,7 @@ app.get("/messages", (req, res) => {
     const latestMsg = messages.filter(message => message.id < 10)
     res.send(latestMsg)
   }
-  if (Object.keys(req.query) === 0) {
+  if (Object.keys(req.query) == 0) {
     res.send(messages)
   }
 })
@@ -57,35 +109,9 @@ app.get("/messages", (req, res) => {
 
 
 
-app.post("/messages", (req, res) => {
-  if (validMessage(req.body)) {
-    const messageContent = req.body
-    const timeSent = new Date()
-    const newMessage = { id: messages.length, from: messageContent.from, text: messageContent.text, timeSent: timeSent }
-    messages.push(newMessage)
-    res.send(newMessage)
-
-  } else {
-    res.status(400).send("Invalid message content.")
-  }
 
 
-})
 
-const validMessage = (message) => {
-  if (message.from === undefined || message.text === undefined || message.from === "" || message.text === "") {
-    return false
-  } else {
-    return true
-  }
-}
-
-app.delete("/messages/:id", (req, res) => {
-  const msgId = req.params.id
-  const index = messages.findIndex(id => messages.id == msgId)
-  messages.splice(index, 1)
-  res.status(202).send({ success: true })
-})
 
 app.listen(3000, () => {
   console.log("Listening on port 3000")
