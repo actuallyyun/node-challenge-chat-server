68dacb4b39ac1a799fdbac54842e308d4a3f9b2f
Yun Ji <this.jiyun@gmail.com>
2022-12-05 09:54:50 +0100

    fetch messages and load correctly

--- fe/src/App.js
+++ fe/src/App.js
@@ -1,16 +1,18 @@
-import logo from './logo.svg';
 import './App.css';
-import React, { UseEffect } from "react"
+import React, { useEffect, useState } from "react"
 
 function App() {
+
+
+
   return (
     <div className="App">
 
       <div className="container">
         <div className="row">
-          <div className="col">
+          <div className="col-6">
             <h2>Latest Messages</h2>
-            <p>Messages</p>
+            <LatestMessages />
           </div>
         </div>
       </div>
@@ -21,6 +23,29 @@ function App() {
 
 
 const LatestMessages = () => {
+  const [messages, setMessages] = useState([])
+  console.log("messages", messages)
+  useEffect(() => {
+    console.log("useEffect called")
 
+    fetch("http://localhost:3000/messages")
+      .then(res => res.json())
+      .then(data => {
+        console.log("data", data)
+        setMessages(data)
+      })
+  }, [])
+
+  return (
+
+    <MessagesList messages={messages} />
+
+  )
+}
+
+const MessagesList = (props) => {
+  const messages = props.messages
+  const listItems = messages.map((message, index) => <li key={index} className="list-group-item">From:{message.from} {message.text}</li>)
+  return <ul className='list-group'>{listItems}</ul>
 }
 export default App;
